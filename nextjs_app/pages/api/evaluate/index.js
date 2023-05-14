2b6f5df71052e0c64b8fdbea8dfb36ea819734ce
Lucas Pauker <lucas.pauker@gmail.com>
2023-05-14 14:39:19 -0700

    Restructure files

--- nextjs_app/pages/api/evaluate/index.js
+++ nextjs_app/pages/api/evaluate/index.js
@@ -75,6 +75,7 @@ export default async function handler(request, response) {
             metricResults: "$metricResults",
             trainingEvaluation: "$trainingEvaluation",
             timeCreated: "$timeCreated",
+            status: "$status",
           }
         }
       ]).toArray();
