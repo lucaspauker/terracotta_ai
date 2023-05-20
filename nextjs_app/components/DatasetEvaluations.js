76c6cfd1014a56b219d3c4d522a5d6526731a24f
Lucas Pauker <lucas.pauker@gmail.com>
2023-05-19 18:30:37 -0700

    Evaluations page UI updages

---
+++ nextjs_app/components/DatasetEvaluations.js
@@ -0,0 +1,195 @@
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router'
+import Link from 'next/link';
+import { Accordion, AccordionSummary, AccordionDetails, Typography, Button } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import CollapseIcon from '@mui/icons-material/Remove';
+import Paper from '@mui/material/Paper';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import axios from 'axios';
+
+import MenuComponent from "components/MenuComponent";
+import { calculateColor, timestampToDateTimeShort } from '/components/utils';
+
+const metricMap = {
+  'f1': 'F1',
+  'bleu': 'BLEU',
+  'rougel': 'RougeL',
+}
+
+function DatasetEvaluations({ datasetNames, evaluations, refreshData }) {
+  const [expanded, setExpanded] = useState(datasetNames);
+  const router = useRouter()
+  const [open, setOpen] = useState(false);
+  const [idToDelete, setIdToDelete] = useState(null);
+
+  const doDelete = () => {
+    axios.post("/api/evaluate/delete/" + idToDelete).then((res) => {
+      setOpen(false);
+      refreshData();
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
+  const handleExpandAll = () => {
+    setExpanded(datasetNames);
+  };
+
+  const handleCollapseAll = () => {
+    setExpanded([]);
+  };
+
+  const handleChange = (datasetName) => {
+    if (expanded.includes(datasetName)) {
+      setExpanded(expanded.filter((name) => name !== datasetName));
+    } else {
+      setExpanded([...expanded, datasetName]);
+    }
+  };
+
+  const handleEdit = (id) => {
+    router.push("/evaluate/edit/" + id);
+  };
+
+  const handleOpen = (id) => {
+    setIdToDelete(id);
+  };
+
+  useEffect(() => {
+    if (idToDelete !== null) {
+      setOpen(true);
+    }
+  }, [idToDelete]);
+
+  return (
+    <div>
+      <div>
+        <Button onClick={handleExpandAll} variant="outlined" color="primary">
+          Expand All
+        </Button>
+        <Button onClick={handleCollapseAll} variant="outlined" color="primary" className='button-margin'>
+          Collapse All
+        </Button>
+      </div>
+      <div className='small-space' />
+      {datasetNames.map((datasetName, index) => (
+        <Box key={datasetName} marginBottom={2}>
+          <Accordion
+            expanded={expanded.includes(datasetName)}
+            onChange={() => handleChange(datasetName)}
+            defaultExpanded="true"
+          >
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography variant="h6">{datasetName}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Paper variant="outlined">
+                <TableContainer>
+                  <Table sx={{ minWidth: 650 }}>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell className='table-cell'>Name</TableCell>
+                        <TableCell className='table-cell'>Date created</TableCell>
+                        <TableCell className='table-cell'>Model name</TableCell>
+                        <TableCell className='table-cell'>Dataset name</TableCell>
+                        <TableCell className='table-cell'>Metrics</TableCell>
+                        <TableCell className='table-cell'></TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {evaluations[datasetName].map((e) => (
+                        <TableRow
+                          key={e._id}
+                          sx={{ '&:last-child td, &:last-child th': { border: 0 }, margin: 0 }}
+                        >
+                          <TableCell>
+                            {e.status === "failed" ?
+                              <div className='horizontal-box flex-start'>
+                                <div>{e.name} &nbsp;&nbsp;&nbsp;</div>
+                                <Tooltip title="Error running evaluation">
+                                  <ErrorIcon sx={{color:'red', fontSize:16}}/>
+                                </Tooltip>
+                              </div>
+                              : e.metricResults ?
+                              <Link className='link' href={"/evaluate/" + e._id}>{e.name}</Link>
+                              :
+                              <div className='horizontal-box flex-start'>
+                                <div>{e.name} &nbsp;&nbsp;&nbsp;</div>
+                                <CircularProgress size={16}/>
+                              </div>
+                            }
+                          </TableCell>
+                          <TableCell>{timestampToDateTimeShort(e.timeCreated)}</TableCell>
+                          <TableCell><Link className='link' href={"/models/" + e.modelId}>{e.modelName}</Link></TableCell>
+                          <TableCell><Link className='link' href={"/data/" + e.datasetId}>{e.datasetName}</Link></TableCell>
+                          <TableCell>
+                            {e.metricResults ?
+                              <div className='metrics-cell'>
+                                {e.metrics.map(m => <div key={m} className='metric-in-table'>
+                                  <span className='metric-in-table-text' style={{backgroundColor: calculateColor(e.metricResults[m])}}>
+                                    {m in metricMap ? metricMap[m] : m}
+                                </span></div>)}
+                              </div>
+                              :
+                              <div className='metrics-cell'>
+                                {e.metrics.map(m => <div key={m} className='metric-in-table'>
+                                  <span className='metric-in-table-text'>
+                                    {m in metricMap ? metricMap[m] : m}
+                                  </span></div>)}
+                              </div>
+                            }
+                          </TableCell>
+                          <TableCell>
+                            <MenuComponent
+                              editFunction={() => handleEdit(e._id)}
+                              deleteFunction={() => handleOpen(e._id)}
+                            />
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </Paper>
+            </AccordionDetails>
+          </Accordion>
+        </Box>
+      ))}
+      <Dialog
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Delete evaluation?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            This action is permanent and cannot be reversed.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpen(false)}>Cancel</Button>
+          <Button onClick={doDelete} autoFocus>
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
+
+export default DatasetEvaluations;
+
