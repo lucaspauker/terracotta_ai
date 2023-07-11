29c5d95f2955bb4993140526624fe0f5ffc90ce3
Lucas Pauker <lucas.pauker@gmail.com>
2023-07-11 17:25:43 -0500

    Update user schema

--- nextjs_app/schemas/User.js
+++ nextjs_app/schemas/User.js
@@ -7,11 +7,9 @@ const userSchema = new Schema({
   },
   firstName: {
     type: String,
-    required: true
   },
   lastName: {
     type: String,
-    required: true
   },
   picture: {
     type: String
